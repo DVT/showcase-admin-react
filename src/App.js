c1a55f7e1a01000a5665d95960feeae4018faadb
Paul <paul.cloete@gmail.com>
2017-05-15 10:14:58 +0200

    View App page update
    -Main page now lists existing apps from firebase
    -React Router installed and set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react';
+import './App.css';
+import * as firebase from "firebase";
+import {Card} from 'react-mdc-web';
+import {CardText} from 'react-mdc-web';
+import {CardHeader} from 'react-mdc-web';
+import {TextField} from 'react-mdc-web';
+import {Fab} from 'react-mdc-web';
+import {Icon} from 'react-mdc-web';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      apps: [],
+      appToExpand: {}
+    }
+  }
+
+  componentDidMount() {  
+    firebase.database().ref("apps/").once('value').then((result) => {
+      var apps = [];
+      var resultVal = result.val();
+      for (var key in resultVal) {
+        if (resultVal.hasOwnProperty(key)) {
+          apps.push(resultVal[key]);
+        }
+      }
+
+      this.setState({
+        apps: apps
+      });
+    });
+  }
+
+  getAppsFromStateAsHtml() {
+    var html = [];
+    for(var i = 0; i < this.state.apps.length; i++) {
+      var app = this.state.apps[i];
+      var index = i;
+      
+        html.push(<Card key={i} className="AppCard">
+        <CardHeader>
+          {app.name}
+        </CardHeader>
+      </Card>);
+      
+    }
+
+    return html;
+  }
+
+  addApp() {
+    
+  }
+
   render() {
     return (
       <div className="App">
-        Hello, World!
+        {this.getAppsFromStateAsHtml()}
+        <Fab className="addAppFab" onClick={this.addApp.bind(this)}><Icon name='create'/></Fab>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from "firebase";
+import 'material-components-web/dist/material-components-web.min.css';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRpZkISgvHK7MQ-sjIdEzhKufnNcJKF8U",
+  authDomain: "dvt-showcase-1b447.firebaseapp.com",
+  databaseURL: "https://dvt-showcase-1b447.firebaseio.com",
+  projectId: "dvt-showcase-1b447",
+  storageBucket: "dvt-showcase-1b447.appspot.com",
+  messagingSenderId: "491015639132"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App}/>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
